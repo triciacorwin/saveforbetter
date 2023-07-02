110d8300cb22303d2f4114313bf7ac2bfc5a8623
Tricia Yeh <triciayeh@Tricias-MacBook-Air.local>
2023-07-02 01:29:46 -0700

    removed redundant directory

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x = 1;
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
